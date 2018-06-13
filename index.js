cb10006a735ba751fff6bac16c01ef4cf51da964
CherylTheUnicorn <37086314+CherylTheUnicorn@users.noreply.github.com>
2018-06-13 16:10:43 +0200

    Aggiornamento portfolio
    ancora cose da aggiustare

--- index.js
+++ index.js
@@ -1,3 +1,44 @@
+var myVar;
+
+function myFunction() {
+    myVar = setTimeout(MostraPagina, 1500);
+}
+
+function MostraPagina() {
+  document.getElementById("spinner1").style.display = "none";
+  document.getElementById("poesia").style.display = "block";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -9,6 +50,6 @@ function scrollFunction() {
 }
 
 function topFunction() {
-    document.body.scrollTop = 0; 
+    document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
